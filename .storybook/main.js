b126ffe824af48bdb3d956e62e9fcbe8feea81a5
Antonio Ballesteros <ToniIAPro73@users.noreply.github.com>
2025-07-06 16:22:02 +0200

    cambios storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
 // .storybook/main.js
 module.exports = {
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',  // o builder-vite si prefieres
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
+
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-essentials',
+    '@storybook/addon-webpack5-compiler-swc',
+    '@chromatic-com/storybook'
   ],
+
+  docs: {
+    autodocs: true
+  }
 };
