2faeff94cfcb1cd6022c3a9b0debe13239708dd4
Antonio Ballesteros <ToniIAPro73@users.noreply.github.com>
2025-07-06 21:05:37 +0200

    Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,18 +2,24 @@
 const path = require('path');
 
 module.exports = {
-  framework: { name: '@storybook/react-webpack5', options: {} },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-webpack5-compiler-swc',
-    '@chromatic-com/storybook'
   ],
-  docs: { autodocs: true },
-
+  docs: {
+    autodocs: true,
+  },
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
+      // Alias raíz: @ === src/
+      '@': path.resolve(__dirname, '../src'),
+      // Alias adicional si prefieres @lib/…
       '@lib': path.resolve(__dirname, '../src/lib'),
     };
     return config;
--- src/components/ui/button/Button.stories.jsx
+++ src/components/ui/button/Button.stories.jsx
@@ -1,15 +1,61 @@
+// src/components/ui/button/Button.stories.jsx
 import React from 'react';
-import Button from './Button';
+import { Button } from './Button.jsx';
 
+/**
+ * Este default export es OBLIGATORIO:
+ * - title: cómo aparecerá en el panel de la izquierda
+ * - component: el componente que vamos a documentar
+ */
 export default {
-  title: 'Components/Button',
+  title: 'UI/Button',
   component: Button,
+  tags: ['autodocs'],         // opcional, para habilitar MDX/autodocs
+  argTypes: {
+    variant: {
+      control: { type: 'select' },
+      options: ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link'],
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['default', 'sm', 'lg', 'icon'],
+    },
+    onClick: { action: 'clicked' },
+  },
 };
 
+/**
+ * Crea un “template” que renderiza tu componente
+ * recibiendo args (props).
+ */
 const Template = (args) => <Button {...args} />;
 
-export const Primario = Template.bind({});
-Primario.args = {
-  children: 'Click me',
-  variant: 'primary',
+/** Historias concretas */
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: 'secondary',
+  children: 'Secondary',
+};
+
+export const Ghost = Template.bind({});
+Ghost.args = {
+  variant: 'ghost',
+  children: 'Ghost',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'lg',
+  children: 'Large Button',
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  size: 'icon',
+  children: <svg width="16" height="16">{/* tu icono */}</svg>,
 };
